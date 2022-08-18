502664e9154d94f7538542b9f2e1f5e2f4129a25
Gabriel Zilioti <gabriel.zilioti@viewb.com.br>
2022-08-18 09:44:12 -0300

    remake

--- musashi/manage.py
+++ musashi/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slimdjango.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musashi.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- musashi/musashi/wsgi.py
+++ musashi/musashi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for slimdjango project.
+WSGI config for musashi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'slimdjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musashi.settings')
 
 application = get_wsgi_application()
