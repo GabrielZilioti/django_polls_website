4cfdee8a5e704829d48b3d70891bbada25cc090f
Gabriel Zilioti <gabriel.zilioti@viewb.com.br>
2022-08-18 10:28:40 -0300

    feat: polls view

--- musashi/musashi/settings.py
+++ musashi/musashi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1eh(zm!!r(pyi1yaqv!#)3!h@!gh_am(704bkh#%_ru=4-_b5v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.125','localhost']
 
 
 # Application definition
