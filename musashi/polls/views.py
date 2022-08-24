ae2ffd5c6bff0471c9b318e25849e79058c6a4da
Gabriel Zilioti <gabriel.zilioti@viewb.com.br>
2022-08-24 08:45:06 -0300

    urls finalizado

--- musashi/polls/views.py
+++ musashi/polls/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 
@@ -19,10 +18,7 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question' : question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
             
